e6ddc69b8a3446dc89e132e3e86361cd67223dfe
方宇 <fangyu.k@bytedance.com>
2022-06-09 15:13:29 +0800

    add bodyy support

--- cbhelper.js
+++ cbhelper.js
@@ -1,5 +1,7 @@
 var Koa = require('koa');
 var Router = require('koa-router');
+const BodyParser = require('koa-bodyparser');
+
 
 var app = new Koa();
 var router = new Router();
@@ -12,6 +14,7 @@ router
   .all('/callback', (ctx, next) => {
     var req = ctx.request;
 
+    var socket = req.socket;
 
     // let headers = req.headers;
     // /** Remve useless headers */
@@ -19,16 +22,16 @@ router
     // delete headers['was-default-hostname'];
     // delete headers['x-arr-log-id'];
 
-    // let output = {
-    //   timestamp : new Date(),
-    //   method    : req.method,
-    //   body      : req.body,
-    //   url       : req.url,
-    //   origin    : req.origin,
-    //   headers   : req.headers,
-    //   params    : req.query
-    // };
-    let output = req;
+    let output = {
+      timestamp : new Date(),
+      method    : req.method,
+      body      : req.body,
+      url       : req.url,
+      origin    : req.origin,
+      headers   : req.headers,
+      params    : req.query
+    };
+    // let output = req;
     /** Remve useless headers */
     delete output.headers['x-site-deployment-id'];
     delete output.headers['was-default-hostname'];
@@ -38,13 +41,17 @@ router
     cbdata = JSON.stringify(output, null, 2);
 
     ctx.response.status = 200;
-    // ctx.body = null;
   })
   .all('/(.*)', (ctx, next)=>{
     ctx.body = 'No Function Found'
   });
 
-app.use(router.routes()).use(router.allowedMethods());
+app
+  .use(BodyParser({
+    enableTypes:['json', 'form', 'text']
+  }))
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 
 var port = (process.env.PORT ||  8080 );
